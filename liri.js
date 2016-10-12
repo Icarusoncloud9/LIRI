b37aa78c40797d67c227646baa97246634e958dd
Your name <Melarajoaquin@gmail.com>
2016-10-12 17:13:03 -0400

    Almost working 100%

--- liri.js
+++ liri.js
@@ -12,7 +12,7 @@ var omdb = require('omdb');
 
 var spotify = require('spotify');
 
-var twitter = require('twitter');
+var Twitter = require('twitter');
 
 // ------------------------------------------------------------------------------------------------
 // State my variables
@@ -22,18 +22,11 @@ var twitter = require('twitter');
 var name;
 
 // Create a list of commands
-var tweets = "my-tweets";
+var commandTweets = "my-tweets";
 
-var spotify = "spotify-this-song";
+var commandSpotify = "spotify-this-song";
 
-var movie = "movie-this";
-
-// Values to store
-
-var song;
-
-var movie;
-
+var commandMovie = "movie-this";
 
 
 // Take two arguments. 
@@ -63,9 +56,10 @@ function greeting(){
 }
 
 // If the "twitter" function is called...
-function twitter(){
+function getTwitter(){
 
-    console.log("Twitter is working!");
+    // Test to see if Twitter is working
+    // console.log("Twitter is working!");
 
     // Paste the sample code from npm
     var client = new Twitter({
@@ -80,88 +74,177 @@ function twitter(){
 
     });
      
-    var params = {screen_name: 'nodejs'};
+    var params = {screen_name: 'FuakataStudios'};
 
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
 
       if (!error) {
 
-        console.log(tweets);
+        for(var i = 0; i < 10; i++) {
+
+            if (tweets[i] == null || tweets[i] == undefined) {
+                break;
+            }
+
+        // console.log(JSON.stringify(tweets[i], null, 2));
+
+        console.log(tweets[i].text);
+            
+        }
 
       }
 
     });
 
-    client.get('favorites/list', function(error, tweets, response) {
+    // client.get('favorites/list', function(error, tweets, response) {
 
-      if(error) throw error;
+    //   if(error) throw error;
 
-      console.log(tweets);  // The favorites. 
+    //   console.log(tweets);  // The favorites. 
 
-      console.log(response);  // Raw response object. 
+    //   console.log(JSON.stringify(response, null, 2));  // Raw response object. 
 
-    });
+    // });
 
 }
 
 // If the "spotify" function is called...
-function spotify(){
+function getSpotify(b){
+
+    // Test to see if Spotify works
+    // console.log("Spotify works!");
 
     // Paste the sample code from npm
-    spotify.search({ type: 'track', query: song }, function(err, data) {
+    spotify.search({ type: 'track', query: b }, function(err, data) {
+
     if ( err ) {
+        
         console.log('Error occurred: ' + err);
+        
         return;
-
-        console.log(JSON.stringify(data, null, 2));
+        
     }
- 
-    // Do something with 'data' 
 
-});
+    // console.log("The raw data: " + JSON.stringify(data, null, 3));
+    
+    for(var i = 0; i < data.tracks.items.length; i++) {
+
+    // Do something with 'data' 
+    console.log("=================================================================");
+
+    console.log("Potential Song Origin ~ " + i);
+    
+    for(var j = 0; j < data.tracks.items[i].artists.length; j++) {
+
+        console.log("Artist involved:   " + data.tracks.items[i].artists[j].name);
+        
+    }
+
+    console.log("Album Name:        " + data.tracks.items[i].album.name);
+
+    console.log("Song Name:         " + value);
+
+    console.log("=================================================================");
+
+    console.log("");
+
+    console.log("");
+
+        }
+    
+    });
 
 }
 
 // If the "movie" function is called
-function movie(){
+function getMovie(b){
+
+    // Test to see if OMDB is working properly;
+    // console.log("OMDB Works!");
 
     // Paste the sample code from npm
-    omdb.search(movie, function(err, movies) {
+    omdb.search(b, function(err, movies) {
+    
     if(err) {
+    
         return console.error(err);
+    
     }
  
     if(movies.length < 1) {
+    
         return console.log('No movies were found!');
+    
     }
  
     movies.forEach(function(movie) {
-        console.log('%s (%d)', movie.title, movie.year);
+        
+        omdb.get({ title: movie.title }, true, function(err, particularMovie) {
+
+            if(err) {
+             
+                return console.error(err);
+            
+            }
+         
+            if(!movie) {
+            
+                return console.log('Movie not found!');
+            
+            }
+
+            // console.log("line 203", particularMovie);
+  
+            console.log("=======================================================================");
+
+            console.log('%s', "Movie: " + particularMovie.title );
+
+            console.log("");
+
+            console.log('%s', "Released on: " + particularMovie.year );
+
+            console.log("");
+
+            console.log('%s', "Rated: " + particularMovie.rated );
+
+            console.log("");
+
+            console.log('%s', "Genres: " + particularMovie.genres );
+
+            console.log("");
+
+            console.log('%s', "Director: " + particularMovie.director );
+
+            console.log("");
+
+            console.log('%s', "Actors: " + particularMovie.actors );
+
+            console.log("");
+
+            console.log('%s', "Plot: " + particularMovie.plot );
+
+            console.log("");
+
+            console.log('%s', "Rating of Movie: " + particularMovie.imdb.rating );
+
+            console.log("");
+
+            console.log("=======================================================================");
+
+            console.log("");
+
+            console.log("");
+
+        });
+        
+
+    
     });
  
-    // Saw (2004) 
-    // Saw II (2005) 
-    // Saw III (2006) 
-    // Saw IV (2007) 
-    // ... 
+
+
 });
  
-omdb.get({ title: 'Saw', year: 2004 }, true, function(err, movie) {
-    if(err) {
-        return console.error(err);
-    }
- 
-    if(!movie) {
-        return console.log('Movie not found!');
-    }
- 
-    console.log('%s (%d) %d/10', movie.title, movie.year, movie.imdb.rating);
-    console.log(movie.plot);
- 
-    // Saw (2004) 7.6/10 
-    // Two men wake up at opposite sides of a dirty, disused bathroom, chained 
-    // by their ankles to pipes. Between them lies... 
-});
 
 }
 
@@ -203,27 +286,16 @@ inquirer.prompt([
 
         message: "What command would you like to execute",
 
-        choices: [tweets, spotify, movie],
+        choices: [commandTweets, commandSpotify, commandMovie],
 
         name: "command"
-
-    },
-
-    {
-        type: "confirm",
-        
-        message: "Are you sure?",
-
-        name: "confirm",
-
-        default: true
 
     }
 
 ]).then(function (user) {
 
     // Figure out what information we are getting back from the user
-    console.log(JSON.stringify(user, null, 2));
+    // console.log(JSON.stringify(user, null, 2));
 
     // This is going to update the global variable action to check which if statement applies to this situation
     action = user.command;
@@ -232,20 +304,22 @@ inquirer.prompt([
     name = user.name;
 
     // If tweets are picked
-    if (action == tweets) {
+    if (action == commandTweets) {
 
         greeting();
 
-        console.log("Here are your latest tweets!");
+        console.log("");
+
+        console.log(name + " Here are your latest tweets!");
 
         console.log("");
 
-        twitter();
+        getTwitter();
 
     }
 
     // If spotify is picked
-    else if (action == spotify) {
+    else if (action == commandSpotify) {
 
         greeting();
 
@@ -257,20 +331,26 @@ inquirer.prompt([
 
             message: "What song would you like to look up?",
 
-            name: "spotify"
+            name: "songName"
 
         }]).then(function(song) {
 
-        console.log("Your spotify song is " + song.spotify)    
+        console.log("");
 
-        song = song.spotify;
+        console.log(name + " Your spotify song is " + song.songName)    
+
+        console.log("");
+
+        value = song.songName;
+
+        getSpotify(song.songName);
 
         });
 
     } 
 
     // If movie is picked
-    else if (action == movie) {
+    else if (action == commandMovie) {
 
         greeting();
 
@@ -286,9 +366,13 @@ inquirer.prompt([
 
         }]).then(function(movie) {
 
-        console.log("Your movie is " + movie.omdb);
+        console.log("");
 
-        movie = movie.omdb;
+        console.log(name + " Your movie is " + movie.omdb);
+
+        console.log("");
+
+        getMovie(movie.omdb);
 
         });
     }
